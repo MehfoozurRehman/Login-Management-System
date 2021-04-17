476b2da9a89f040a555a0e3657f4fdbd3fbb778a
Mehfooz-ur-Rehman <mehfoozijaz786@gmail.com>
2021-04-17 19:02:42 +0500

    commit

---
+++ src/components/App.js
@@ -0,0 +1,34 @@
+import React from "react";
+import SignUp from "./SignUp";
+import Dashboard from "./Dashboard";
+import Login from "./Login";
+import UpdateProfile from "./UpdateProfile";
+import ForgotPassword from "./ForgotPassword";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container } from "react-bootstrap";
+import { AuthProvider } from "../context/AuthContext";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import PrivateRoute from "./PrivateRoute";
+
+export default function App() {
+  return (
+    <Container
+      className="d-flex align-items-center justify-content-center"
+      style={{ minHeight: "100vh" }}
+    >
+      <div className="w-100" style={{ maxWidth: "400px" }}>
+        <Router>
+          <AuthProvider>
+            <Switch>
+              <PrivateRoute exact path="/" component={Dashboard} />
+              <PrivateRoute path="/update-profile" component={UpdateProfile} />
+              <Route path="/signup" component={SignUp} />
+              <Route path="/login" component={Login} />
+              <Route path="/forgot-password" component={ForgotPassword} />
+            </Switch>
+          </AuthProvider>
+        </Router>
+      </div>
+    </Container>
+  );
+}
